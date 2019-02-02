b6354d523b870a2b4df263c49e0ca2c110e49f51
Daiki Chifu <qwerty.0139.5284@gmail.com>
2019-02-03 08:56:12 +0900

    phpのchapter3,4です

---
+++ php/chapter4/practice.php
@@ -0,0 +1,26 @@
+<?php
+
+$message = "hello, world!";
+var_dump($message);
+
+$number1 = 3;
+$number2 = 3.321;
+
+var_dump($number1);
+var_dump($number2);
+
+$judge1 = true;
+$judge2 = false;
+
+var_dump($judge1);
+var_dump($judge2);
+
+$array = [1,2,3];
+
+var_dump($array);
+
+$null = null;
+
+var_dump($null);
+
+?>
