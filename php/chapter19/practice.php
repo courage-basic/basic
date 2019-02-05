671807af915b49ca02173c616c4471ca27d9e13f
Daiki Chifu <qwerty.0139.5284@gmail.com>
2019-02-06 06:21:41 +0900

    名前空間でエラーが起きます。

--- php/chapter19/practice.php
+++ php/chapter19/practice.php
@@ -1,7 +1,14 @@
 <?php
 
- require "user.php";
- require_once "user.php";
+ // require "user.php";
+ // require_once "user.php";
+
+ // include "user.php";
+ // include_once "user.php";
+
+spl_autoload_register(function($class) {
+  require $class.".php";
+});
 
 $user_obj = new User("Tanaka");
 $user_obj->say_hello();
--- php/chapter20/practice.php
+++ php/chapter20/practice.php
@@ -1,5 +1,19 @@
 <?php
 
+// require "user.php";
+// require "test.php";
+require_once "user.php";
+require_once "test.php";
 
+use Courage\User;
+use Courage\Test;
+
+// $user_obj = new Courage\User\User("Tanaka");
+// $user_obj = new Courage\Test\User("Tanaka");
+
+$user_obj = new User\User("Tanaka");
+// $user_obj = new Test\User("Tanaka");
+
+$user_obj->say_hello();
 
 ?>
